fc3d9c4aa63cd274ec6b021c29d8eb538b008db3
Kenneth Ulloa <ulloakenth@gmail.com>
2023-06-08 09:55:03 -0500

    Actividades

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
@@ -1,18 +1,13 @@
 package com.example.movilessoftware2023a
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+class ACicloVida : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
-        botonCicloVida.setOnClickListener {
-            irActividad(ACicloVida::class.java)
-        }
+        setContentView(R.layout.activity_aciclo_vida)
     }
 
     fun irActividad(
