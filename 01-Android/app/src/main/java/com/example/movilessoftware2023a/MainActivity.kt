ccbd5062dbeb16cca37557e63fdc719593bde169
KennethUlloa <ulloakenth@gmail.com>
2023-06-29 10:16:30 -0500

    Base de datos SQLite, Helpers

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.movilessoftware2023a
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
@@ -15,40 +16,38 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ){
-            result -> {
-                if(result.resultCode == Activity.RESULT_OK) {
-                    if(result.data != null) {
-                        val data = result.data
-                        "${data?.getStringExtra("nombreModificado")}"
-                    }
+                result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    // Logica Negocio
+                    val data = result.data
+                    "${data?.getStringExtra("nombreModificado")}"
                 }
             }
         }
 
     val callbackIntentPickUri =
         registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult(),
-            {
-                result -> {
-                    if (result.resultCode == RESULT_OK) {
-                        if(result.data != null) {
-                            if(result.data!!.data != null) {
-                                var uri = result.data!!.data!!
-                                val cursor = contentResolver.query(uri, null, null, null, null, null)
-                                cursor?.moveToFirst()
-                                val indiceTelefono = cursor?.getColumnIndex(
-                                    ContactsContract.CommonDataKinds.Phone.NUMBER
-                                )
-
-                                val telefono = cursor?.getString(indiceTelefono!!)
-                                cursor?.close()
-                                "Telefono $telefono"
-                            }
-                        }
+            ActivityResultContracts.StartActivityForResult()
+        ){
+                result ->
+            if(result.resultCode === RESULT_OK){
+                if(result.data != null){
+                    if(result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(uri, null, null, null,  null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        "Telefono ${telefono}"
                     }
                 }
             }
-        )
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -63,12 +62,14 @@ class MainActivity : AppCompatActivity() {
         }
 
         val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
-        botonIntentImplicito.setOnClickListener {
-            val intentRespuesta = Intent(
-                Intent.ACTION_PICK,
-                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
-            )
-        }
+        botonIntentImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
 
         val botonExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
         botonExplicito.setOnClickListener {
