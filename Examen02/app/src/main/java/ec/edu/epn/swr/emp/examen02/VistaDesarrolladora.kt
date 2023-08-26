2d0ea7e7f2eae610bd5f25d2222224c1893f47cd
KennethUlloa <ulloakenth@gmail.com>
2023-08-26 16:32:02 -0500

    Examen 2: Firebase y paso ente actividades

--- Examen02/app/src/main/java/ec/edu/epn/swr/emp/examen02/VistaDesarrolladora.kt
+++ Examen02/app/src/main/java/ec/edu/epn/swr/emp/examen02/VistaDesarrolladora.kt
@@ -2,10 +2,19 @@ package ec.edu.epn.swr.emp.examen02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+import ec.edu.epn.swr.emp.examen02.utils.CambiadorActividad
 
 class VistaDesarrolladora : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vista_desarrolladora)
+
+        val listViewVistaDesarrolladora = findViewById<ListView>(R.id.lv_desarrolladoras)
+        val botonCrear = findViewById<Button>(R.id.btn_crear_desarrolladora)
+        botonCrear.setOnClickListener {
+            CambiadorActividad.cambiarActividad(this, EdicionDesarrolladora::class.java)
+        }
     }
 }
